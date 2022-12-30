a746954f8c7b14aa39e74e1a35b40a0ddc7feb2c
Adrián Sánchez Albanell <adrian.sanchez.albanell@gmail.com>
2022-12-30 15:42:59 +0100

    Delete src/components/00_layouts directory

--- src/components/00_layouts/list/listItem/ListItem.tsx
+++
@@ -1,9 +0,0 @@
-import { FC } from 'react';
-
-import { Props } from 'src/types/core';
-
-const ListItem: FC<Props> = ({ children }) => {
-    return <li>{children}</li>;
-};
-
-export default ListItem;
